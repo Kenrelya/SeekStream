9525b62ece7ead63c55ea1564db8f6f6d7bbf769
Leosten <leonard.leungchongwo@gmail.com>
2016-11-20 21:32:49 +0100

    fix check livestream

--- src/app/directives/main-stream/main-stream.controller.js
+++ src/app/directives/main-stream/main-stream.controller.js
@@ -1,27 +1,31 @@
 angular.module('seekstream').controller('MainStreamController', function ($interval, $http, $sce) {
     var vm = this;
 
+    vm.loading = true;
     vm.twitch_online = false;
     vm.hitbox_online = false;
 
     vm.user_infos = vm.userinfos;
     vm.mylink     = "https://player.twitch.tv/?channel=" + vm.user_infos.twitch_username;
     vm.streams    = $sce.trustAsResourceUrl(vm.mylink);
-    vm.interval_time = 15000;
+    vm.interval_time = 8000;
 
     vm.startCheckOnline = $interval(function() {
         $http.jsonp(
             'https://api.twitch.tv/kraken/streams/' + vm.user_infos.twitch_username + '?client_id=3q7fxx94uudt0r51zgf7sigu64fccvl&callback=JSON_CALLBACK')
             .success(function(response) {
-                if (response.stream !== null) {
-                    vm.interval_time = 50000;
-                    vm.twitch_online = true;
-
+                if (response.status === 404) {
+                    vm.twitch_online = false;
+                } else if (response.stream === null) {
+                    vm.twitch_online = false;
+                    vm.loading = false;
                 } else {
                     if (angular.isDefined(vm.startCheckOnline)) {
                         $interval.cancel(vm.startCheckOnline);
                     }
-                    vm.twitch_online = false;
+                    vm.interval_time = 50000;
+                    vm.loading = false;
+                    vm.twitch_online = true;
                 }
             });
     }, vm.interval_time);
